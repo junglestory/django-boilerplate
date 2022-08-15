980c1ddf6680102e49eefb640cdca70f37982ccc
junglestory <monocaine@gmail.com>
2022-08-15 22:08:15 +0900

    db 정보 추가

--- django_boilerplate/settings.py
+++ django_boilerplate/settings.py
@@ -82,6 +82,17 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'boilerplate',
+        'USER': 'root',
+        'PASSWORD': 'root1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
