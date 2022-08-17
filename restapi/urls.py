e0777fd375d7b4058c4040f5f01b7c7a23de1f36
junglestory <monocaine@gmail.com>
2022-08-17 14:15:38 +0900

     전체 조회 기능 추가

--- restapi/urls.py
+++ restapi/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path("hello/", hello),
+    path("news/", news),
 ]
--- restapi/views.py
+++ restapi/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import News
+from .serializers import NewsSerializer
 import logging
 
 logger = logging.getLogger(__name__)
@@ -9,3 +11,11 @@ logger = logging.getLogger(__name__)
 @api_view(['GET'])
 def hello(request):
     return Response("Hello World!")
+
+
+@api_view(['GET'])
+def news(request):
+    datas = News.objects.all()
+    serializer = NewsSerializer(datas, many=True)
+
+    return Response(serializer.data, content_type=u"application/json; charset=utf-8")    
